1a425abbaa01d55538507cc6bda1a098ca37e1f3
Oula Kuuva <oula.kuuva@iki.fi>
2025-04-01 10:39:47 +0300

    feat(pkgs): add btop

--- flake.nix
+++ flake.nix
@@ -26,6 +26,7 @@
       # $ nix-env -qaP | grep wget
       environment.systemPackages = [
         pkgs.alejandra # nix linter
+        pkgs.btop
         pkgs.carapace
         pkgs.cargo
         pkgs.curl
