c687b7924cd00c6d515332c666f7e687b44f8253
Oula Kuuva <oula.kuuva@iki.fi>
2026-06-06 00:29:56 +0300

    chore: update mise

--- flake.nix
+++ flake.nix
@@ -36,7 +36,7 @@
     };
     # TODO: go back to unstable once direnv starts behaving again
     mise-nixpkgs.url = "github:NixOS/nixpkgs/16c7794d0a28b5a37904d55bcca36003b9109aaa";
-    mise.url = "github:jdx/mise?ref=v2026.4.5";
+    mise.url = "github:jdx/mise?ref=v2026.6.1";
     mise.inputs.nixpkgs.follows = "mise-nixpkgs"; # the default rustc is too old
   };
 
