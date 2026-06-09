8844cc1fc8548590358c8e7e0e70d95d683508cf
Oula Kuuva <oula.kuuva@iki.fi>
2026-06-09 13:50:39 +0300

    fix(pkgs): pin jjui to 0.10.2
    Forward slash does not trigger filtering in keyboard shortcut search
    anymore in 0.10.3, making the whole thing useless. Should fix that in
    jjui but this is good enough for now.

--- flake.nix
+++ flake.nix
@@ -77,7 +77,17 @@
         gnupg
         gnutar
         imagemagick
-        jjui
+        (jjui.overrideAttrs (old: rec {
+          version = "0.10.2";
+          src = fetchFromGitHub {
+            owner = "idursun";
+            repo = "jjui";
+            tag = "v${version}";
+            hash = "sha256-VTaOd5LSBxo6EhTyjoyAqX+wTEcm88qIgUCcd+TRYY4=";
+          };
+          vendorHash = "sha256-GDYgZI6X7UwnyKXOJVmqXXtm4ulA10uuX5MeqKVTheA=";
+          ldflags = ["-X main.Version=${version}"];
+        }))
         jujutsu
         k9s
         kanata-with-cmd
